8a58f9ea211e55c1cd09de025ebe2a7cc7160588
Andrew Polk <andrew_polk@sil.org>
2023-08-23 17:01:35 -0700

    Further improve PDF generation

--- src/pdf-generation/make-pdf.sh
+++ src/pdf-generation/make-pdf.sh
@@ -12,11 +12,23 @@ else
     URL="https://sillsdev.github.io/paratext-manual/$1/Overview"
 fi
 
+# By generating the PDFs directly in the build dir, we can avoid having to build the
+# docusaurus site all over again in the workflow.
+# Also, if we generate the PDFs in the static folder,
+# the docusaurus build copies all of them to each locale by default, so we get duplicates.
+if [ "$1" == "en" ]; then
+    mkdir -p "build/downloads"
+    OUTPUTPATH="build/downloads/Ptx-man-a5-$1-9.3.pdf"
+else
+    mkdir -p "build/$1/downloads"
+    OUTPUTPATH="build/$1/downloads/Ptx-man-a5-$1-9.3.pdf"
+fi
+
 npx docu-pdf $URL \
 --coverPath="src/pdf-generation/pdf-covers/cover-man-$1.html" \
 --contentSelector="article" \
 --pageSize="A5" \
---outputPath="static/downloads/Ptx-man-a5-$1-9.3.pdf" \
+--outputPath="$OUTPUTPATH" \
 --disableTOC \
 --headerTemplate="<footer />" \
 --pdfMargin="40,40,80,40" \
