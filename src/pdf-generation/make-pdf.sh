3fb0970de659f73254dbb8bb88287df139bd1104
JenniBeadle <108683165+JenniBeadle@users.noreply.github.com>
2025-03-20 01:01:19 +0800

    Update make-pdf.sh version 9.5

--- src/pdf-generation/make-pdf.sh
+++ src/pdf-generation/make-pdf.sh
@@ -19,10 +19,10 @@ fi
 # the docusaurus build copies all of them to each locale by default, so we get duplicates.
 if [ "$1" == "en" ]; then
     mkdir -p "build/downloads"
-    OUTPUTPATH="build/downloads/Ptx-man-a5-$1-9.4.pdf"
+    OUTPUTPATH="build/downloads/Ptx-man-a5-$1-9.5.pdf"
 else
     mkdir -p "build/$1/downloads"
-    OUTPUTPATH="build/$1/downloads/Ptx-man-a5-$1-9.4.pdf"
+    OUTPUTPATH="build/$1/downloads/Ptx-man-a5-$1-9.5.pdf"
 fi
 
 npx docu-pdf $URL \
